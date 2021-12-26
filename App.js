af61aa55e94c0a80314cdd608b7e0dc25276838e
victor bobkov <victorbobkov@gmail.com>
2021-12-27 02:34:28 +0300

    finished the game logic

--- App.js
+++ App.js
@@ -9,9 +9,14 @@ export default function App() {
    const [userNumber, setUserNumber] = useState()
    const [guessRounds, setGuessRounds] = useState(0)
 
+   const configureNewGameHandler = () => {
+      setGuessRounds(0)
+      setUserNumber(null)
+
+   }
+
    const startGameHandler = (selectedNumber) => {
       setUserNumber(selectedNumber)
-      setGuessRounds(0)
    }
 
    const gameOverHandler = (numOfRounds) => {
@@ -21,11 +26,20 @@ export default function App() {
    let content = <StartGameScreen onStartGame={startGameHandler} />
 
    if (userNumber && guessRounds <= 0) {
-      content = <GameScreen
-         userChoice={userNumber} onGameOver={gameOverHandler}
-      />
+      content = (
+         <GameScreen
+            userChoice={userNumber}
+            onGameOver={gameOverHandler}
+         />
+      )
    } else if (guessRounds > 0) {
-      content = <GameOverScreen />
+      content = (
+         <GameOverScreen
+            roundsNumber={guessRounds}
+            userNumber={userNumber}
+            onRestart={configureNewGameHandler}
+         />
+      )
    }
 
    return (
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
 
 const GameOverScreen = (props) => {
    return (
       <View style={styles.screen}>
          <Text>Игра окончена!</Text>
+         <Text>Количество раундов: {props.roundsNumber}</Text>
+         <Text>Загаданное число: {props.userNumber}</Text>
+         <Button title='НОВАЯ ИГРА' onPress={props.onRestart} />
       </View>
    )
 }
