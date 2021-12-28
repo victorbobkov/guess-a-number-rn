df92542716bee615aeb39f8eeb68a61dc1179c85
victor bobkov <victorbobkov@gmail.com>
2021-12-28 23:23:39 +0300

    updated Text Component

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,23 +1,53 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button, Image } from 'react-native'
+import { View, StyleSheet, Button, Image, Text } from 'react-native'
 import BodyText from '../components/BodyText'
 import TitleText from '../components/TitleText'
+import Colors from '../constants/colors'
 
 const GameOverScreen = (props) => {
+   let rounds
+   if (props.roundsNumber === 1) {
+      rounds = 'раунд'
+   } else if (props.roundsNumber >= 2 && props.roundsNumber <= 4) {
+      rounds = 'раунда'
+   } else {
+      rounds = 'раундов'
+   }
+
    return (
       <View style={styles.screen}>
          <TitleText>Игра окончена!</TitleText>
          <View style={styles.imageContainer}>
             <Image
                fadeDuration={1000}
-               // source={require('../assets/success.png')}
-               source={{uri: 'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'}}
+               source={require('../assets/success.png')}
                style={styles.image}
                resizeMode='cover'
             />
          </View>
-         <BodyText>Количество раундов: {props.roundsNumber}</BodyText>
-         <BodyText>Загаданное число: {props.userNumber}</BodyText>
+         <View style={styles.resultContainer}>
+            {/*<BodyText>*/}
+            {/*   Компьютеру понадобилось*/}
+            {/*   <Text*/}
+            {/*      style={styles.highlight}> {props.roundsNumber}*/}
+            {/*   </Text> {rounds},*/}
+            {/*</BodyText>*/}
+            {/*<BodyText>*/}
+            {/*   чтобы отгадать число*/}
+            {/*   <Text*/}
+            {/*      style={styles.highlight}> {props.userNumber}*/}
+            {/*   </Text>*/}
+            {/*</BodyText>*/}
+
+            <BodyText style={styles.resultText}>
+               Компьютеру понадобилось{' '}
+               <Text style={styles.highlight}>{props.roundsNumber}</Text> {rounds},
+               чтобы отгадать число{' '}
+               <Text style={styles.highlight}>{props.userNumber}</Text>
+            </BodyText>
+         </View>
+
+
          <Button title='НОВАЯ ИГРА' onPress={props.onRestart} />
       </View>
    )
@@ -41,6 +71,18 @@ const styles = StyleSheet.create({
    image: {
       width: '100%',
       height: '100%',
+   },
+   highlight: {
+      color: Colors.primary,
+      fontFamily: 'open-sans-bold',
+   },
+   resultContainer: {
+      width: '80%',
+      marginVertical: 10
+   },
+   resultText: {
+      textAlign: 'center',
+      fontSize: 15
    },
 })
 
